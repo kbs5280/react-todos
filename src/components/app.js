e47d96dc67cab24726ca760fcf8788d63f04c1ec
Kris Sparks <kbs5280@gmail.com>
2016-11-29 10:18:08 -0700

    Add todos list and todos list header components
    Components render locally.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import TodosList from './todos-list'
+
+const todos = [
+{
+  task: 'make React tutorial',
+  isCompleted: false
+},
+{ task: 'eat dinner',
+  isCompleted: true
+}
+];
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>React ToDos App</h1>
+        <TodosList />
       </div>
     );
   }
